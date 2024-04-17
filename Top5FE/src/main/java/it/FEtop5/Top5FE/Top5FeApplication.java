214db59535f24a41096a7bb1cd99d0541c14fccc
antoniolacolla <antonio.lacolla@edu.unife.it>
2024-04-17 16:09:06 +0200

    spring

---
+++ Top5FE/src/main/java/it/FEtop5/Top5FE/Top5FeApplication.java
@@ -0,0 +1,13 @@
+package it.FEtop5.Top5FE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top5FeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top5FeApplication.class, args);
+	}
+
+}
---
+++ Top5FE/src/test/java/it/FEtop5/Top5FE/Top5FeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.FEtop5.Top5FE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top5FeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
