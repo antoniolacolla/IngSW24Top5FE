ce61df57126841a34008eb13596c312ff7514331
antoniolacolla <antonio.lacolla@edu.unife.it>
2024-04-18 16:27:10 +0200

    Update App.vue

--- Top5FE/vue-app/src/App.vue
+++ Top5FE/vue-app/src/App.vue
@@ -8,13 +8,13 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Ciao!" />
+      <HelloWorld msg="trimone!" />
     </div>
   </header>
 
   <main>
     <div>
-      Ciao
+      Fanculo
     </div>
     <TheWelcome />
   </main>
