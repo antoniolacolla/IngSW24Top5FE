59983422744cf6405599974ceb59cc4cb19cd712
AntoninoCostantino <antonino.costantino@edu.unife.it>
2024-04-18 18:37:00 +0200

    mappa

--- Top5FE/vue-app/src/App.vue
+++ Top5FE/vue-app/src/App.vue
@@ -1,50 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="trimone!" />
-    </div>
-  </header>
-
-  <main>
-    <div>
-      Fanculo
-    </div>
-    <TheWelcome />
-  </main>
+  <div id="map" style="height: 500px;"></div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    this.initMap();
+  },
+  methods: {
+    initMap() {
+      const mapOptions = {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 0
+      };
+      const mapElement = document.getElementById('map');
+      new google.maps.Map(mapElement, mapOptions);
+    }
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style>
+#map {
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  width: 100%; /* Assicurati che la mappa occupi tutta la larghezza possibile */
 }
 </style>
