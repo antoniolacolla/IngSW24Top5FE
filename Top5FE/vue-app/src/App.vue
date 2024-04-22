c41a8561bcdfb62b35c77215fc66bb032a96e60d
AntoninoCostantino <antonino.costantino@edu.unife.it>
2024-04-22 19:29:54 +0200

    trasporti

--- Top5FE/vue-app/src/App.vue
+++ Top5FE/vue-app/src/App.vue
@@ -2,6 +2,9 @@
   <div id="map" style="height: 500px;"></div>
 </template>
 
+
+
+
 <script>
 import * as maptilersdk from '@maptiler/sdk';
 
@@ -9,14 +12,10 @@ maptilersdk.config.apiKey = 'I4M3OisYlYJWkLCFDwoa';
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element to render the map
   style: maptilersdk.MapStyle.STREETS,
-  center: [16.62662018, 49.2125578], // starting position [lng, lat]
-  zoom: 14, // starting zoom
+ 
 });
 </script>
 
 <style>
-#map {
 
-  width: 100%; /* Assicurati che la mappa occupi tutta la larghezza possibile */
-}
 </style>
