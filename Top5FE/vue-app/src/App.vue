36e58c5a141408442c39db932145bddb80fd8bfd
AntoninoCostantino <antonino.costantino@edu.unife.it>
2024-04-24 15:44:40 +0200

    mappa aggiornata con bottone funzionante

--- Top5FE/vue-app/src/App.vue
+++ Top5FE/vue-app/src/App.vue
@@ -12,7 +12,8 @@ maptilersdk.config.apiKey = 'I4M3OisYlYJWkLCFDwoa';
 const map = new maptilersdk.Map({
   container: 'map', // container's id or the HTML element to render the map
   style: maptilersdk.MapStyle.STREETS,
- 
+  center: [16.62662018, 49.2125578], // starting position [lng, lat]
+  zoom: 14, // starting zoom
 });
 </script>
 
