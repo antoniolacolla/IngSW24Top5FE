cdce369a0ed2914bc397cc871a9f0f0175451504
antoniolacolla <antonio.lacolla@edu.unife.it>
2024-05-28 16:42:52 +0200

    Update vite.config.js

--- map/vite.config.js
+++ map/vite.config.js
@@ -1,7 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  server:{
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true
+        }
+      }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
